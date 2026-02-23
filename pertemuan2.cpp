c7d38462848bbfe863656efa66077ae4a5105f3d
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-02-23 21:41:27 +0700

    menambahkan panjang dan lebar

--- pertemuan2.cpp
+++ pertemuan2.cpp
@@ -2,4 +2,13 @@
 using namespace std;
 
 int main(){ //mulai
-    int p, l, luas;}
+    int p, l, luas;
+
+    //langkah 2
+    cout << "Masukan Panjang : ";
+    cin >> p;
+
+    //langkah 3
+    cout << "Masukan Lebar : ";
+    cin >> l;
+}
