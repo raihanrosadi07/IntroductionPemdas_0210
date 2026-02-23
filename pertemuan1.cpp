a5ccf85c5e6eb3de14b588db753dde872c80ba48
raihanrosadi07 <raihanrosadi284@gmail.com>
2026-02-23 21:32:50 +0700

    end

--- pertemuan1.cpp
+++ pertemuan1.cpp
@@ -3,3 +3,13 @@ using namespace std;
 
 //variabel global
 int sks;
+
+int main(){
+    //variabel lokal
+    int umur;
+    umur = 20;
+
+    cout << "Halo Monyet Ucup" << endl;
+    cout << 10 << endl;
+    cout << umur;
+}
